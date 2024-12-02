3497b518d730318d0ead929ec8e7951facdf2bcb
Matyan <158287738+Mattikitsme@users.noreply.github.com>
2024-12-02 19:23:40 +0500

    4_5.py

--- lab4/4_5.py
+++ lab4/4_5.py
@@ -1,10 +1,15 @@
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
 zoo.insert(1, "bear")
 print(zoo)
+
+birds = ['sparrow', 'parrot', 'eagle']
+
 for i in birds:
     zoo.append(i)
 print(zoo)
+
 zoo.pop(zoo.index("elephant"))
 print(zoo)
+
 for i in ["lion", "lark"]:
-    print(zoo.index(i))
+    print(zoo.index(i) if i in zoo else f"{i} не найдено в зоопарке")
