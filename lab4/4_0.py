8f70f70c9621d38f0946f679cae60d9ae1ccd70f
Matyan <158287738+Mattikitsme@users.noreply.github.com>
2024-12-02 19:20:28 +0500

    4_0.py

--- lab4/4_0.py
+++ lab4/4_0.py
@@ -3,12 +3,16 @@ sites = {
     'London': (510, 510),
     'Paris': (480, 480),
 }
+
 distances = {}
+
 def dist(i, j):
     x1, y1 = sites[i]
     x2, y2 = sites[j]
-    return ((x1 - x2) 2 + (y1 - y2) 2) ** 0.5
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
 names = sites.keys()
 for i in names:
     distances[i] = dict((j, dist(i, j)) for j in names)
+
 print(distances)
