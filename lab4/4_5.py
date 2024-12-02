d7e8da536606fa34e2c2c5eabad848385240387a
Matyan <158287738+Mattikitsme@users.noreply.github.com>
2024-12-02 19:39:36 +0500

    4_5.py

--- lab4/4_5.py
+++ lab4/4_5.py
@@ -1,15 +1,27 @@
-zoo = ['lion', 'kangaroo', 'elephant', 'monkey']
-zoo.insert(1, "bear")
-print(zoo)
+def f():
+    zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 
-birds = ['sparrow', 'parrot', 'eagle']
+    zoo.insert(1, "bear")
+    print(zoo)
 
-for i in birds:
-    zoo.append(i)
-print(zoo)
+    birds = ['rooster', 'ostrich', 'lark', ]
+    for i in birds:
+        zoo.append(i)
+    print(zoo)
 
-zoo.pop(zoo.index("elephant"))
-print(zoo)
+    zoo.pop(zoo.index("elephant"))
+    print(zoo)
 
-for i in ["lion", "lark"]:
-    print(zoo.index(i) if i in zoo else f"{i} не найдено в зоопарке")
+    for i in ["lion", "lark"]:
+        print(zoo.index(i))
+
+f()
+
+def test(capfd):
+    f()
+    out, err = capfd.readouterr()
+    assert out == "['lion', 'bear', 'kangaroo', 'elephant', 'monkey']\n\
+['lion', 'bear', 'kangaroo', 'elephant', 'monkey', 'rooster', 'ostrich', 'lark']\n\
+['lion', 'bear', 'kangaroo', 'monkey', 'rooster', 'ostrich', 'lark']\n\
+0\n\
+6\n"
